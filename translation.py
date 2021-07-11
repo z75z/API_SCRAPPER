567f1dd13ddfefeb9a05c19d2bf83ff0ef061a96
loverboyXD <79841865+loverboyXD@users.noreply.github.com>
2021-07-11 08:02:55 +0530

    Add files via upload

---
+++ translation.py
@@ -0,0 +1,16 @@
+class Translation(object):
+    START_TEXT = """H!✨
+    I am APP_ID AND API_HASH Scrapper by PsychoBots...
+    Enter Your **Phone no.** To get Your APP_ID/API_HASH From my.telegram.org
+    
+    Join support To Report Bugs and issues
+
+/start at any stage to re-enter your details"""
+    AFTER_RECVD_CODE_TEXT = """Nice👀
+   Now send the Telegram code that you received from Telegram!
+   That code will provide you your API_ID+API_HASH. from my.telegram.org """
+    BEFORE_SUCC_LOGIN = "recieved code. Scarpping web page ..."
+    ERRED_PAGE = "something wrongings. failed to get app id. \n\n@PsychoBots_chat"
+    CANCELLED_MESG = "Bye! Please re /start the bot conversation"
+    IN_VALID_CODE_PVDED = "sorry, but the input does not seem to be a valid Telegram Web-Login code"
+    IN_VALID_PHNO_PVDED = "sorry, but the input does not seem to be a valid phone number"
