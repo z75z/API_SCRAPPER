300efc331535101252802cbab3691aae5b7b8acc
Dev-v2 <89397215+Team-ShadowZ@users.noreply.github.com>
2021-11-01 16:17:37 +0530

    </> @Master_X_Updates </>

--- translation.py
+++ translation.py
@@ -1,6 +1,6 @@
 class Translation(object):
     START_TEXT = """H!✨
-    I am APP_ID AND API_HASH Scrapper by PsychoBots...
+    I am APP_ID AND API_HASH Scrapper by Master_X_Updates...
     Enter Your **Phone no.** To get Your APP_ID/API_HASH From my.telegram.org
     
     Join support To Report Bugs and issues
@@ -10,7 +10,7 @@ class Translation(object):
    Now send the Telegram code that you received from Telegram!
    That code will provide you your API_ID+API_HASH. from my.telegram.org """
     BEFORE_SUCC_LOGIN = "recieved code. Scarpping web page ..."
-    ERRED_PAGE = "something wrongings. failed to get app id. \n\n@PsychoBots_chat"
+    ERRED_PAGE = "something wrongings. failed to get app id. \n\n@Master_X_Updates"
     CANCELLED_MESG = "Bye! Please re /start the bot conversation"
     IN_VALID_CODE_PVDED = "sorry, but the input does not seem to be a valid Telegram Web-Login code"
     IN_VALID_PHNO_PVDED = "sorry, but the input does not seem to be a valid phone number"
